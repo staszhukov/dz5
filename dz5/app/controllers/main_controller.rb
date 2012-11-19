7c018bde2506d1bec6c8166ddd0df8ff98ef3d5d
user <user@test.ru>
2012-11-19 19:37:49 +0400

    Initial commit

---
+++ dz5/app/controllers/main_controller.rb
@@ -0,0 +1,13 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def help
+  end
+
+  def contacts
+  end
+end
---
+++ dz5/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dz5::Application.config.secret_token = '380203762aa4cee08bf722db5ebeb1cca9cf57bc6d8831e2567fdaddd31e61c964b01fc59b0997cfbb691928fcda9de8badd76c0407175d5482ce46d8c4c48f5'
